3ba766449c5df34fb6dcf33af31c363e04fe0fa4
Vallisha M <vallishamohan@gmail.com>
2023-01-01 15:34:49 +0530

    final

--- src/DataStreamer.ts
+++ src/DataStreamer.ts
@@ -1,31 +1,36 @@
 export interface Order {
-  price: number,
-  size: number,
+  price: number
+  size: number
 }
 export interface ServerRespond {
-  stock: string,
-  top_bid: Order,
-  top_ask: Order,
-  timestamp: Date,
+  stock: string
+  top_bid: Order
+  top_ask: Order
+  timestamp: Date
+  price_def: number
+  price_abc: number
+  trigger_alert: number
+  upper_bound: number
+  lower_bound: number
 }
 
 class DataStreamer {
-  static API_URL: string = 'http://localhost:8080/query?id=1';
+  static API_URL: string = "http://localhost:8080/query?id=1"
 
   static getData(callback: (data: ServerRespond[]) => void): void {
-    const request = new XMLHttpRequest();
-    request.open('GET', DataStreamer.API_URL, false);
+    const request = new XMLHttpRequest()
+    request.open("GET", DataStreamer.API_URL, false)
 
     request.onload = () => {
       if (request.status === 200) {
-        callback(JSON.parse(request.responseText));
+        callback(JSON.parse(request.responseText))
       } else {
-        alert ('Request failed');
+        alert("Request failed")
       }
     }
 
-    request.send();
+    request.send()
   }
 }
 
-export default DataStreamer;
+export default DataStreamer
